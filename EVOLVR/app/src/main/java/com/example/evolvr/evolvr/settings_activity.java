589bf363ab57fc74b7dab06f2d3a0a6617550b52
Wesley Howell <wjhowell@umich.edu>
2015-11-22 23:16:00 -0500

    back buttons
    (back buttons)

--- EVOLVR/app/src/main/java/com/example/evolvr/evolvr/settings_activity.java
+++ EVOLVR/app/src/main/java/com/example/evolvr/evolvr/settings_activity.java
@@ -1,15 +1,29 @@
 package com.example.evolvr.evolvr;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.View.OnClickListener;
 
-public class settings_activity extends AppCompatActivity {
+
+public class settings_activity extends AppCompatActivity
+implements OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        View v1 = findViewById(R.id.backbutton);
+        v1.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View arg0) {
+        if(arg0.getId() == R.id.backbutton){
+            Intent intent = new Intent(this,InitialScreen.class);
+            this.startActivity(intent);
+        }
+
     }
 }
