7040a2a6a8251910592762b46231143d93e4f1c4
Wesley Howell <wjhowell@umich.edu>
2015-11-22 22:53:55 -0500

    buttons!
    settings and highscores button on main menu should work

--- EVOLVR/app/src/main/java/com/example/evolvr/evolvr/InitialScreen.java
+++ EVOLVR/app/src/main/java/com/example/evolvr/evolvr/InitialScreen.java
@@ -1,13 +1,37 @@
 package com.example.evolvr.evolvr;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class InitialScreen extends AppCompatActivity {
+public class InitialScreen extends AppCompatActivity
+implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+        setContentView(R.layout.activity_main_menu);
+        View v1 = findViewById(R.id.highscoresbutton);
+        v1.setOnClickListener(this);
+        View v2 = findViewById(R.id.settingsbutton);
+        v2.setOnClickListener(this);
+    }
+
+    //override the OnClickListener interface method
+    @Override
+    public void onClick(View arg0) {
+        if(arg0.getId() == R.id.highscoresbutton){
+            Intent intent = new Intent(this,highscores_activity.class);
+            this.startActivity(intent);
+        }
+        if(arg0.getId() == R.id.settingsbutton){
+            Intent intent = new Intent(this,settings_activity.class);
+            this.startActivity(intent);
+        }
     }
 }
+
---
+++ EVOLVR/app/src/main/java/com/example/evolvr/evolvr/highscores_activity.java
@@ -0,0 +1,15 @@
+package com.example.evolvr.evolvr;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class highscores_activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highscores);
+    }
+}
