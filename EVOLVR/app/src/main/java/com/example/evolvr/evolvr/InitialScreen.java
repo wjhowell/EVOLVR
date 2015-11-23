e13f4cdd618d2c2d2a2c5657a13e65844a32fa8d
Becno <bnards@umich.edu>
2015-11-22 21:23:28 -0500

    broken_stuff
    It still broke

--- EVOLVR/app/src/main/java/com/example/evolvr/evolvr/InitialScreen.java
+++ EVOLVR/app/src/main/java/com/example/evolvr/evolvr/InitialScreen.java
@@ -8,6 +8,6 @@ public class InitialScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_initial_screen);
+        setContentView(R.layout.activity_home_screen);
     }
 }
