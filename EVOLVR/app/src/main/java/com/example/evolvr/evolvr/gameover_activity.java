64cb36af4ed60056537c76551b1626fdcadae536
anashif <anashif@umich.edu>
2015-12-10 19:19:01 -0500

    Game Over screen uses custom font

--- EVOLVR/app/src/main/java/com/example/evolvr/evolvr/gameover_activity.java
+++ EVOLVR/app/src/main/java/com/example/evolvr/evolvr/gameover_activity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -15,14 +16,21 @@ implements OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameover);
-        View v1 = findViewById(R.id.backbutton);
+        Button v1 = (Button) findViewById(R.id.backbutton);
         v1.setOnClickListener(this);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/retro.ttf");
+        v1.setTypeface(font);
         TextView txt = (TextView) findViewById(R.id.textView3);
         txt.setTypeface(font);
+        TextView txt2 = (TextView) findViewById(R.id.textView4);
+        txt2.setTypeface(font);
+        TextView txt3 = (TextView) findViewById(R.id.textView5);
+        txt3.setTypeface(font);
         TextView scoreTxt = (TextView) findViewById(R.id.textView6);
+        scoreTxt.setTypeface(font);
         int score = savedInstanceState.getInt("Score");
         scoreTxt.setText(Integer.toString(score));
+        txt.setTypeface(font);
     }
 
     @Override
