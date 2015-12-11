f5ed8056d3a5e93f3d420777dc0d531e827f0e5d
anashif <anashif@umich.edu>
2015-12-10 19:46:45 -0500

    Pause screen uses custom fonts

--- EVOLVR/app/src/main/java/com/example/evolvr/evolvr/pause_activity.java
+++ EVOLVR/app/src/main/java/com/example/evolvr/evolvr/pause_activity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class pause_activity extends AppCompatActivity implements View.OnClickListener {
@@ -16,13 +17,31 @@ public class pause_activity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pause_menu);
-        View v1 = findViewById(R.id.resumebutton);
+        Button v1 = (Button) findViewById(R.id.resumebutton);
         v1.setOnClickListener(this);
-        View v2 = findViewById(R.id.mainmenu);
+        Button v2 = (Button) findViewById(R.id.mainmenu);
         v2.setOnClickListener(this);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/retro.ttf");
         TextView txt = (TextView) findViewById(R.id.textView3);
         txt.setTypeface(font);
+        v1.setTypeface(font);
+        v2.setTypeface(font);
+
+        TextView diff = (TextView) findViewById(R.id.textView6);
+        diff.setTypeface(font);
+        TextView easy = (TextView) findViewById(R.id.radioButton6);
+        easy.setTypeface(font);
+        TextView medium = (TextView) findViewById(R.id.radioButton7);
+        medium.setTypeface(font);
+        TextView hard = (TextView) findViewById(R.id.radioButton8);
+        hard.setTypeface(font);
+
+        TextView mode = (TextView) findViewById(R.id.textView7);
+        mode.setTypeface(font);
+        TextView staticMode = (TextView) findViewById(R.id.radioButton9);
+        staticMode.setTypeface(font);
+        TextView dynamicMode = (TextView) findViewById(R.id.radioButton10);
+        dynamicMode.setTypeface(font);
     }
 
     @Override
