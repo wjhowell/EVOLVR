00e78de638b9c098d72ed1563e3080f2317e592b
anashif <anashif@umich.edu>
2015-12-10 19:47:15 -0500

    Settings screen uses custom fonts

--- EVOLVR/app/src/main/java/com/example/evolvr/evolvr/settings_activity.java
+++ EVOLVR/app/src/main/java/com/example/evolvr/evolvr/settings_activity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -17,11 +18,35 @@ implements OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
-        View v1 = findViewById(R.id.backbutton);
+        Button v1 = (Button) findViewById(R.id.backbutton);
         v1.setOnClickListener(this);
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/retro.ttf");
+        v1.setTypeface(font);
         TextView txt = (TextView) findViewById(R.id.textView3);
         txt.setTypeface(font);
+        TextView txt2 = (TextView) findViewById(R.id.textView4);
+        txt2.setTypeface(font);
+
+        TextView diff = (TextView) findViewById(R.id.textView);
+        diff.setTypeface(font);
+        TextView easy = (TextView) findViewById(R.id.radioButton);
+        easy.setTypeface(font);
+        TextView medium = (TextView) findViewById(R.id.radioButton2);
+        medium.setTypeface(font);
+        TextView hard = (TextView) findViewById(R.id.radioButton3);
+        hard.setTypeface(font);
+
+        TextView mode = (TextView) findViewById(R.id.textView2);
+        mode.setTypeface(font);
+        TextView staticMode = (TextView) findViewById(R.id.radioButton4);
+        staticMode.setTypeface(font);
+        TextView dynamicMode = (TextView) findViewById(R.id.radioButton5);
+        dynamicMode.setTypeface(font);
+
+        TextView username = (TextView) findViewById(R.id.textView5);
+        username.setTypeface(font);
+        TextView input = (TextView) findViewById(R.id.editText);
+        input.setTypeface(font);
     }
     @Override
     public void onClick(View arg0) {
